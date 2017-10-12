30cc23508d1a9e556576e77a2686f2f3f682aea5
asim <asimawan.42@gmail.com>
2017-10-12 16:01:13 +0500

    Rename DOMAIN to DOMAIN_NAME

--- deploy-all.sh
+++ deploy-all.sh
@@ -8,12 +8,12 @@ sed "s|<%CERT-ARN%>|${CERT_ARN}|g" "${tmpl_path}/nginx-ingress-controller-servic
 
 tmpl_path="external/exposecontroller"
 echo "Substituting domain in expose-controller-config-map.tmpl.yaml"
-sed "s|<%DOMAIN%>|${DOMAIN}|g" "${tmpl_path}/expose-controller-config-map.tmpl.yaml" > "${tmpl_path}/expose-controller-config-map.yaml"
+sed "s|<%DOMAIN%>|${DOMAIN_NAME}|g" "${tmpl_path}/expose-controller-config-map.tmpl.yaml" > "${tmpl_path}/expose-controller-config-map.yaml"
 tmpl_path="internal/exposecontroller"
-sed "s|<%DOMAIN%>|${DOMAIN}|g" "${tmpl_path}/expose-controller-config-map.tmpl.yaml" > "${tmpl_path}/expose-controller-config-map.yaml"
+sed "s|<%DOMAIN%>|${DOMAIN_NAME}|g" "${tmpl_path}/expose-controller-config-map.tmpl.yaml" > "${tmpl_path}/expose-controller-config-map.yaml"
 echo "Substituting domain in externaldns-deployment.tmpl.yaml"
 tmpl_path="externaldns"
-sed "s|<%DOMAIN%>|${DOMAIN}|g" "${tmpl_path}/externaldns-deployment.tmpl.yaml" > "${tmpl_path}/externaldns-deployment.yaml"
+sed "s|<%DOMAIN%>|${DOMAIN_NAME}|g" "${tmpl_path}/externaldns-deployment.tmpl.yaml" > "${tmpl_path}/externaldns-deployment.yaml"
 
 echo "Substituting txt-owner-id in externaldns-deployment.tmpl.yaml"
 sed "s|<%TXT-OWNER-ID%>|${TXT_OWNER_ID}|g" "${tmpl_path}/externaldns-deployment.tmpl.yaml" > "${tmpl_path}/externaldns-deployment.yaml"
