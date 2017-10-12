a730952f8a069085975111b8c1540d6049352b79
asim <asimawan.42@gmail.com>
2017-10-12 15:47:48 +0500

    Interpolate parameters using sed

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+tmpl_path="internal/ingress"
+echo "Substituting cert arn in nginx-ingress-controller-service.tmpl.yaml"
+sed "s|<%CERT-ARN%>|${CERT_ARN}|g" "${tmpl_path}/nginx-ingress-controller-service.tmpl.yaml" > "${tmpl_path}/nginx-ingress-controller-service.yaml"
+tmpl_path="external/ingress"
+sed "s|<%CERT-ARN%>|${CERT_ARN}|g" "${tmpl_path}/nginx-ingress-controller-service.tmpl.yaml" > "${tmpl_path}/nginx-ingress-controller-service.yaml"
+
+tmpl_path="external/exposecontroller"
+echo "Substituting domain in expose-controller-config-map.tmpl.yaml"
+sed "s|<%DOMAIN%>|${DOMAIN}|g" "${tmpl_path}/expose-controller-config-map.tmpl.yaml" > "${tmpl_path}/expose-controller-config-map.yaml"
+tmpl_path="internal/exposecontroller"
+sed "s|<%DOMAIN%>|${DOMAIN}|g" "${tmpl_path}/expose-controller-config-map.tmpl.yaml" > "${tmpl_path}/expose-controller-config-map.yaml"
+echo "Substituting domain in externaldns-deployment.tmpl.yaml"
+tmpl_path="externaldns"
+sed "s|<%DOMAIN%>|${DOMAIN}|g" "${tmpl_path}/externaldns-deployment.tmpl.yaml" > "${tmpl_path}/externaldns-deployment.yaml"
+
+echo "Substituting txt-owner-id in externaldns-deployment.tmpl.yaml"
+sed "s|<%TXT-OWNER-ID%>|${TXT_OWNER_ID}|g" "${tmpl_path}/externaldns-deployment.tmpl.yaml" > "${tmpl_path}/externaldns-deployment.yaml"
+
 cd externaldns
 ./deploy-externaldns.sh
 cd ..
